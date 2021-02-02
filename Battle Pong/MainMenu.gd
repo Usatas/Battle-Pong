802ac79ecdec47b27d78b1596b21b594d201d0e5
Frederic Aust <frederic.aust@hs-bochum.de>
2021-02-02 17:10:26 +0100

    MainMenu: * Grobe Erstellung des Hauptmenues mit Buttons zum starten verschiedener Spielmodi

---
+++ Battle Pong/MainMenu.gd
@@ -0,0 +1,38 @@
+extends Control
+
+var rendering_enabled = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
+
+
+func _on_but_local_two_player_pressed():
+    pass # Replace with function body.
+
+
+func _on_but_two_re_with_images_pressed():
+    pass # Replace with function body.
+
+
+func _on_but_two_re_with_position_pressed():
+    pass # Replace with function body.
+
+
+func _on_cb_rendering_enabled_pressed():
+    rendering_enabled = not rendering_enabled
+    print(rendering_enabled)
+    pass # Replace with function body.
+
+
+func _on_but_trainer_with_images_pressed():
+    pass # Replace with function body.
+
+
+func _on_but_trainer_with_position_pressed():
+    pass # Replace with function body.
